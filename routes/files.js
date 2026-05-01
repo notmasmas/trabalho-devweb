8e36e4c9311c1d4dd51b92ac8849c3afadde5257
Mas <mariahelenammelo23@gmail.com>
2026-05-01 16:44:28 -0300

    testing Multer middleware for file upload

--- routes/files.js
+++ routes/files.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {getAllFiles, createFile, deleteFile, editFile} = require('../controllers/file');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: './uploads/'});
 
 router.route('/')
     .get(getAllFiles)
-    .post(createFile);
+    .post(upload.single('file'), createFile);
 
 router.route('/:id')
     .delete(deleteFile)
