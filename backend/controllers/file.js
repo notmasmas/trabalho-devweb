1367d68e892d9bcd5f79cb38ef283840a07eac33
Mas <mariahelenammelo23@gmail.com>
2026-05-14 15:14:53 -0300

    refactor: using new error classes in controller

--- backend/controllers/file.js
+++ backend/controllers/file.js
@@ -1,151 +1,107 @@
 const File = require('../models/file.js');
+const {StatusCodes} = require('http-status-codes');
+const {BadRequestError, NotFoundError, CustomAPIError} = require('../errors');
 
 const getAllFiles = async (req, res) => {
+
+    const {name, author, uploader} = req.query;
+    const queryObject = {}
+
+    if (name) {
+        queryObject.name = {$regex: name, $options: 'i'};
+    }
+    if (author) {
+        queryObject.author = {$regex: author, $options: 'i'};
+    }
+    if (uploader) {
+        queryObject.uploader = {$regex: uploader, $options: 'i'};
+    }
+
+    let result = File.find(queryObject);
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const files = await result;
     
-    try {
-        const {name, author, uploader} = req.query;
-        const queryObject = {}
-
-        if (name) {
-            queryObject.name = {$regex: name, $options: 'i'};
-        }
-        if (author) {
-            queryObject.author = {$regex: author, $options: 'i'};
-        }
-        if (uploader) {
-            queryObject.uploader = {$regex: uploader, $options: 'i'};
-        }
-
-        let result = File.find(queryObject);
-
-        const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 10;
-        const skip = (page - 1) * limit;
-
-        result = result.skip(skip).limit(limit);
-
-        const files = await result;
-        
-        res
-            .status(200)
-            .json({files})
-    }
-    catch (error) {
-        res
-            .status(201)
-            .json({'msg': error});
-    }
+    res
+        .status(StatusCodes.OK)
+        .json({files})
 }
 
 const createFile = async (req, res) => {
-    
-    try {
-
-        if (!req.file) {
-            return res.status(400).json({'msg': 'File must be provided'});
-        }
-
-        const {size, path} = req.file;
-        const {name, author, uploader, description, protected} = req.body;
-
-        const fileData = {
-            name,
-            author,
-            uploader,
-            description,
-            protected,
-            fileSize: size,
-            fileURI: path
-        }
-
-        const file = await File.create(fileData);
-
-        res
-            .status(201)
-            .json(file);
+    if (!req.file) {
+        throw BadRequestError('File must be provided');
     }
-    catch (error) {
-        res
-            .status(500)
-            .json({'msg': error});   
+
+    const {size, path} = req.file;
+    const {name, author, uploader, description, protected} = req.body;
+
+    const fileData = {
+        name,
+        author,
+        uploader,
+        description,
+        protected,
+        fileSize: size,
+        fileURI: path
     }
+
+    const file = await File.create(fileData);
+
+    res
+        .status(StatusCodes.CREATED)
+        .json(file);
 }
 
 const deleteFile = async (req, res) => {
 
-    try {
-        const {id:fileID} = req.params;
-        const file = await File.deleteOne({_id: fileID});
+    const {id:fileID} = req.params;
+    const file = await File.deleteOne({_id: fileID});
 
-        if (!file) {
-            return res
-                .status(404)
-                .json({'msg': 'File not found'});
-        }
+    if (!file) {
+        throw NotFoundError('File not found')
+    }
 
-        res
-            .status(200)
-            .json(file);
-    }
-    catch (error) {
-        res
-            .status(500)
-            .json({'msg': error});   
-    }
+    res
+        .status(200)
+        .json(file);
 }
 
 const editFile = async (req, res) => {
 
-    try {
-        const{id:fileID} = req.params;
-        const file = await File.findByIdAndUpdate({_id: fileID}, req.body, {new: true});
+    const{id:fileID} = req.params;
+    const file = await File.findByIdAndUpdate({_id: fileID}, req.body, {returnDocument: 'after'});
 
-        if (!file) {
-            return res
-                .status(404)
-                .json({'msg': 'File not found'});
-        }
-
-        res
-            .status(200)
-            .json(file);
+    if (!file) {
+        throw NotFoundError('File not found');
+        console.log('Got here');
     }
-    catch (error) {
 
-        res
-            .status(500)
-            .json({'msg': error});
-    }
+    res
+        .status(StatusCodes.OK)
+        .json(file);
 }
 
 const downloadFile = async (req, res) => {
 
-    try {
-        const {id:fileID} = req.params;
-        const {fileURI} = await File.findOne({_id: fileID}, {fileURI: 1});
+    const {id:fileID} = req.params;
+    const {fileURI} = await File.findOne({_id: fileID}, {fileURI: 1});
 
-        if (!fileURI) {
-            return res
-                .status(404)
-                .json({'msg': 'File not found'});
-        }
-
-        res
-            .status(200)
-            .download(fileURI, 'file.pdf', (error) => {
-
-                if (error) {
-                   return res
-                        .status(500)
-                        .json({'msg': error.msg});
-                }
-            })
+    if (!fileURI) {
+        throw NotFoundError('File not found');
     }
-    catch (error) {
-        res
-            .status(500)
-            .json({'msg': error})
-    }
+
+    res
+        .status(StatusCodes.OK)
+        .download(fileURI, 'file.pdf', (error) => {
+            if (error) {
+                throw CustomAPIError('Something went wrong. Try again later!')
+            }
+        })
 }
 
 module.exports = {
--- backend/errors/custom-api-error.js
+++ backend/errors/custom-api-error.js
@@ -1,6 +1,9 @@
+const {StatusCodes} = require('http-status-codes');
+
 class CustomAPIError extends Error {
     constructor(message) {
         super(message);
+        this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
     }
 }
 
